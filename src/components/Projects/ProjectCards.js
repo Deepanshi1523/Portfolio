a86b2bbdb3639550d7544424fbbeb94058f67316
Deepanshi1523 <104934054+Deepanshi1523@users.noreply.github.com>
2023-09-21 00:39:44 +0530

    added description to projects

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -15,6 +15,7 @@ function ProjectCards(props) {
         </Card.Text>
         <Button variant="primary" href={props.ghLink} target="_blank">
           <BsGithub /> &nbsp;
+          {"GitHub"}
         </Button>
         {"\n"}
         {"\n"}
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -33,7 +33,7 @@ function Projects(){
                         <ProjectCard
                             imgPath={tindog}
                             title="Tindog"
-                            description="It is a food ordering static website with amazing UI. It is a beginner friendly website. It is also dynamic in nature for every screen size. In this project I have used the following languages-HTML, CSS and Javascript."
+                            description="It is a website for Dog's dating app. It is static in nature with some amazing effects. In this I have used HTML and CSS. This project idea is taken from tinder but here dogs date each other instead of humans"
                             ghLink="https://github.com/Deepanshi1523/tindog.github.io"
                             demoLink="https://deepanshi1523.github.io/tindog.github.io/"
                         />
@@ -42,7 +42,7 @@ function Projects(){
                         <ProjectCard
                             imgPath={simonGame}
                             title="Simon Game"
-                            description="It is a food ordering static website with amazing UI. It is a beginner friendly website. It is also dynamic in nature for every screen size. In this project I have used the following languages-HTML, CSS and Javascript."
+                            description="This project is based on simon game in which you have to memorise the sequence of buttons in which they are played and then you have to play in same sequence and if you press the wrong button then the game is over. It is a basic javascript project which is based on event listeners and randomisation."
                             ghLink="https://github.com/Deepanshi1523/simongame.github.io"
                             demoLink="https://deepanshi1523.github.io/simongame.github.io/"
                         />
@@ -51,7 +51,7 @@ function Projects(){
                         <ProjectCard
                             imgPath={drumkit}
                             title="Drum Kit"
-                            description="It is a food ordering static website with amazing UI. It is a beginner friendly website. It is also dynamic in nature for every screen size. In this project I have used the following languages-HTML, CSS and Javascript."
+                            description="It is a minor javascript project in which you can play various musical instruments via key specified on them or by mouse click. It is used by toddlers to get familiar with the music. It is based on event listeners property of javascript"
                             ghLink="https://github.com/Deepanshi1523/drumKit.github.io"
                             demoLink="https://deepanshi1523.github.io/drumKit.github.io/"
                         />
@@ -60,7 +60,7 @@ function Projects(){
                         <ProjectCard
                             imgPath={diceGame}
                             title="Dice Challenge"
-                            description="It is a food ordering static website with amazing UI. It is a beginner friendly website. It is also dynamic in nature for every screen size. In this project I have used the following languages-HTML, CSS and Javascript."
+                            description="It is a two man game where only one can win. In this we generate a random number on both dice. The number is in the range of 1-6 same as that of a real dice. The player with highest number wins the game. This project is based on event listeners and randomisation property of the javascript."
                             ghLink="https://github.com/Deepanshi1523/diceChallenge.github.io"
                             demoLink="https://deepanshi1523.github.io/diceChallenge.github.io/"
                         />
